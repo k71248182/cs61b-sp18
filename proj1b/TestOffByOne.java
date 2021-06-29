71ef0c5c32ee9d4e34f7e2338d60f5cfc3296898
Tina <tina.j.toronto@gmail.com>
2021-06-29 08:53:01 +0800

    fixed testing for upper/lower case

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -14,7 +14,7 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
-        assertFalse(offByOne.equalChars('A', 'a'));
+        assertFalse(offByOne.equalChars('A', 'b'));
         assertTrue(offByOne.equalChars('&', '%'));
         assertFalse(offByOne.equalChars('&', '&'));
         assertTrue(offByOne.equalChars('1', '2'));
