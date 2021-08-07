aa6f8b32b5231c02333c9b107274b2c03928b4f9
Tina <tina.j.toronto@gmail.com>
2021-08-07 08:36:29 +0800

    added cycle check

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -10,15 +10,52 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int[] invisibleEdgeTo;
+    private boolean cycleFound = false;
+
     public MazeCycles(Maze m) {
         super(m);
+        invisibleEdgeTo = new int[maze.V()];
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(1);
     }
 
-    // Helper methods go here
+    /** Helper methods: for every visited vertex v,
+     * if there is an adjacent u such that u is already visited
+     * and u is not parent of v, then there is a cycle in graph.
+     */
+    private void dfs(int v) {
+        marked[v] = true;
+        announce();
+        for (int neighbor : maze.adj(v)) {
+            if (cycleFound) {
+                return;
+            }
+            if (!marked[neighbor]) {
+                invisibleEdgeTo[neighbor] = v;
+                dfs(neighbor);
+            } else if (neighbor != invisibleEdgeTo[v]) {
+                cycleFound = true;
+                drawCycle(neighbor, v);
+            }
+        }
+    }
+
+    /** Update edgeTo and announce so that the cycle
+     * will be drawn on the canvas.
+     */
+    private void drawCycle(int start, int end) {
+        int v = end;
+        while (v != start) {
+            edgeTo[v] = invisibleEdgeTo[v];
+            announce();
+            v = invisibleEdgeTo[v];
+        }
+        edgeTo[start] = end;
+        announce();
+    }
 }
 
